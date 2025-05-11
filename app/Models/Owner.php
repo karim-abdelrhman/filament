08b5571c33c242b18a3d5bdb33f3301d455595c0
karim abdelrhman <kabdo2536@gmail.com>
2025-05-11 16:57:52 +0300

    Patient Resource

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Owner extends Model
 {
+    protected $fillable = ['name' , 'email' , 'phone'];
     public function patients(): HasMany
     {
         return $this->hasMany(Patient::class);
